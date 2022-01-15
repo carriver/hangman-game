2ecce0edadff942a740e6d0dafded48ae67210da
Carlos Rivera <carlos.rivera.link@gmail.com>
2022-01-15 16:57:07 -0500

    Added controller, scope and data-binding to implement game functionality

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const app = angular.module('HangmanGame',[]);
 
-app.controller('ctrl', $scope => {
+app.controller('ctrl',['$scope','$timeout', function($scope, $timeout) {
         
-    $scope.wrongLettersSelected = [];
-	$scope.correctLettersSelected = [];
+    $scope.wrongLettersSubmitted = [];
+	$scope.correctLettersSubmitted = [];
     $scope.guesses = 6;
 	$scope.displayWord = '';
 	$scope.input = {
@@ -17,16 +17,67 @@ app.controller('ctrl', $scope => {
 		return words[index];
     }
 
-    const newHangmanGame = () => {
-        $scope.wrongLettersSelected = [];
-        $scope.correctLettersSelected = [];
-        $scope.guesses = 6;
-        $scope.displayWord = '';
+    const selectedWord = randomWord();
 
-        selectedWord = randomWord();
-        console.log(selectedWord);
+    const newHangmanGame = () => {
+        $scope.wrongLettersSubmitted = [];
+        $scope.correctLettersSubmitted = [];
+        $scope.guesses = 6;
+        $scope.displayWord = selectedWord.split('').reduce((acc, val) => acc.concat('*'), '');
+    }
+    
+    $scope.letterSubmitted = () => {
+        for (let index = 0; index < $scope.wrongLettersSubmitted.length; index++) {
+            if ($scope.wrongLettersSubmitted[index].toUpperCase()
+                == $scope.input.letter.toUpperCase()){
+                $scope.input.letter = '';
+                return;
+            }           
+        }
+
+        for (let index = 0; index < $scope.correctLettersSubmitted.length; index++) {
+            if ($scope.correctLettersSubmitted[index].toUpperCase()
+                == $scope.input.letter.toUpperCase()){
+                $scope.input.letter = '';
+                return;
+            }           
+        }
+
+        let correct = false;
+        for (let index = 0; index < selectedWord.length; index++) {
+            if (selectedWord[index].toUpperCase() == $scope.input.letter.toUpperCase()) {
+                $scope.displayWord = 
+                $scope.displayWord.slice(0,index)
+                +$scope.input.letter.toUpperCase()
+                +$scope.displayWord.slice(index+1);
+                correct = true;
+            }            
+        }
+
+        if (correct) {
+			$scope.correctLettersSubmitted.push($scope.input.letter.toUpperCase());
+		} else {
+			$scope.guesses--;
+			$scope.wrongLettersSubmitted.push($scope.input.letter.toUpperCase());
+		}
+        
+        $scope.input.letter='';
+
+        if($scope.guesses == 0) {
+            alert('You lost!');
+            $timeout(function() {
+                newHangmanGame();
+            },500);
+        }
+
+        if($scope.displayWord.indexOf("*") == -1) {
+            alert('You won!');
+            $timeout(function() {
+                newHangmanGame();
+            },500);
+        }
     }
 
     newHangmanGame();
      
-});
+}]);
